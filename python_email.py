8717da4c6e7f2367f513d72589035a88e206cee8
wyp8711 <wyp8711@gmail.com>
2020-02-09 20:30:28 +0800

    Add files via upload

---
+++ python_email.py
@@ -0,0 +1,25 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.message import EmailMessage
+
+msg = EmailMessage()
+msg.set_content(
+"""測試訊息
+盤後資訊""")
+
+msg['Subject'] = "股票資訊"
+msg['From'] = ['stock@info.com']#傳送的電子郵件
+msg['To'] = ['test@gmail.com']#接收的電子郵件
+
+# Send the message via our own SMTP server.
+try:
+	smtpObj = smtplib.SMTP('msa.hinet.net',25)#SMTP伺服器
+#	smtpObj.login(account_mail,password)
+	smtpObj.send_message(msg)
+	print("Successfully sent email")
+except Exception:  
+	print("Error: unable to send email")
+smtpObj.quit()
+
